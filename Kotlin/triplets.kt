822e964436cc4c426bffd7e95ebb6cf79b05697f
Jealousmango <jealousmango@gmail.com>
2017-08-01 20:02:15 -0700

    Folder structure to accomodate multiple language solutions. Setup for Kotlin solution.

---
+++ Kotlin/triplets.kt
@@ -0,0 +1,13 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    val alice = 0
+    val bob = 0
+
+    val arr:IntArray = intArrayOf(a0, a1, a2)
+    val ar:IntArray = intArrayOf(b0, b1, b2)
+
+    println(arr)
+}
